1c6e566b7bcf5c67f1aef759dfb0948f8208cd4a
Andrej <xbelkoa@stuba.sk>
2024-02-19 21:11:16 +0100

    feat: added comments to code

--- main.py
+++ main.py
@@ -4,26 +4,44 @@ from fei.ppds import Thread, Semaphore, print
 
 
 class Shared:
+    """This class represents shared data."""
     def __init__(self, semaphore):
+        """Initializes shared data."""
         self.semaphore = semaphore
 
 
 def sleeping(person):
+    """This function simulates sleeping of the person.
+
+    Parameters:
+    - person (str): The name of the person sleeping.
+    """
     print(f'{person} is sleeping')
     sleep(5)
 
 
 def hygiene(person):
+    """This function simulates person doing hygiene.
+
+    Parameters:
+    - person (str): The name of the person doing hygiene.
+    """
     print(f'{person} is doing hygiene')
     sleep(2)
 
 
 def eating(person):
+    """This function simulates eating of the person.
+
+    Parameters:
+    - person (str): The name of the person eating.
+    """
     print(f'{person} is eating')
     sleep(2)
 
 
 def jano(shared):
+    """This function simulates Jano's morning routine."""
     sleeping('Jano')
     hygiene('Jano')
     eating('Jano')
@@ -32,6 +50,7 @@ def jano(shared):
 
 
 def fero(shared):
+    """This function simulates Fero's morning routine."""
     sleeping('Fero')
     hygiene('Fero')
     shared.semaphore.wait()

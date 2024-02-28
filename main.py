50355c2699928d2aac184470e46f944330c11842
Andrej <xbelkoa@stuba.sk>
2024-02-28 21:45:11 +0100

    feat : created functions run, board, unboard, load and unload

--- main.py
+++ main.py
@@ -46,7 +46,27 @@ class Barrier:
         self.turnstile.wait()
 
 
-def train():
+def load():
+    print("Passengers are boarding.")
+
+
+def unload():
+    print("Passengers are unboarding.")
+
+
+def run():
+    print("Train is running.")
+
+
+def board(i):
+    print(f"Passenger {i} is boarding")
+
+
+def unboard(i):
+    print(f"Passenger {i} is unboarding")
+
+
+def train(shared):
     """Train the """
     while True:
         load()
@@ -59,20 +79,37 @@ def train():
         shared.unboardQ.signal(C)
         shared.unboarded.wait()
 
-def passenger():
+
+def passenger(i, shared):
     """Passenger """
     while True:
         shared.boardQ.wait()
         board()
-        shared.boardB.wait(shared.boarded.signal()) #bariera
+        shared.boardB.wait(shared.boarded.signal())  # bariera
 
         shared.unboardQ.wait()
         unboard()
-        shared.unboardB.wait(shared.unboarded.signal()) #bariera
+        shared.unboardB.wait(shared.unboarded.signal())  # bariera
 
 
 def main():
-    print("Hello World!")
+    """Main function"""
+    boardQ = Semaphore(0)
+    boarded = Semaphore(0)
+    unboardQ = Semaphore(0)
+    unboarded = Semaphore(0)
+
+    ready_cnt = 0
+    turnstile = Semaphore(0)
+    barrier_mutex = Mutex()
+    boardB = Barrier(ready_cnt, turnstile, barrier_mutex)
+
+    ready_cnt2 = 0
+    turnstile2 = Semaphore(0)
+    barrier_mutex2 = Mutex()
+    unboardB = Barrier(ready_cnt2, turnstile2, barrier_mutex2)
+
+    shared = Shared(boardQ, boarded, unboardQ, unboarded, boardB, unboardB)
 
 
 if __name__ == '__main__':
